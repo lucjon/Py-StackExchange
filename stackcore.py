142d3c55ec8b90d27e2b6511f77a7d7e5dabe185
Lucas Jones <lucas@lucasjones.co.uk>
2010-07-03 14:53:29 +0100

    Factored out some utility classes.

---
+++ stackcore.py
@@ -0,0 +1,159 @@
+# stackcore.py - JSONModel/Enumeration + other utility classes that don't really belong now that the API's multi-file
+# This file is safe to "import *" - it doesn't even
+# import anything itself.
+
+## JSONModel base class
+class JSONModel(object):
+	"""The base class of all the objects which describe API objects directly - ie, those which take JSON objects as parameters to their constructor."""
+
+	def __init__(self, json, site, skip_ext=False):
+		self.json_ob = DictObject(json)
+		self.site = site
+
+		for f in [x for x in self.transfer if hasattr(self.json_ob, x)]:
+			setattr(self, f, getattr(self.json_ob, f))
+
+		if hasattr(self, '_extend') and not skip_ext:
+			self._extend(self.json_ob, site)
+
+	def fetch(self):
+		"""Fetches all the data that the model can describe, not just the attributes which were specified in the original response."""
+		if hasattr(self, 'fetch_callback'):
+			res = self.fetch_callback(self, self.site)
+
+			if isinstance(res, dict):
+				self.__init__(res, self.site)
+			elif hasattr(res, 'json_ob'):
+				self.__init__(res.json_ob, self.site)
+			else:
+				raise ValueError('Supplied fetch callback did not return a usable value.')
+		else:
+			return False
+	
+	# Allows the easy creation of updateable, partial classes
+	@classmethod
+	def partial(cls, fetch_callback, site, populate):
+		"""Creates a partial description of the API object, with the proviso that the full set of data can be fetched later."""
+
+		model = cls({}, site, True)
+		
+		for k, v in populate.iteritems():
+			setattr(model, k, v)
+
+		model.fetch_callback = fetch_callback
+
+	# for use with Lazy classes that need a callback to actually set the model property
+	def _up(self, a):
+		"""Returns a function which can be used with the LazySequence class to actually update the results properties on the model with the
+new fetched data."""
+
+		def inner(m):
+			setattr(self, a, m)
+		return inner
+
+class Enumeration(object):
+	"""Provides a base class for enumeration classes. (Similar to 'enum' types in other languages.)"""
+
+	@classmethod
+	def from_string(cls, text, typ=None):
+		if typ is not None:
+			if hasattr(typ, '_map') and text in typ._map:
+				return getattr(typ, typ._map[text])
+			elif hasattr(typ, text[0].upper() + text[1:]):
+				return getattr(typ, text[0].upper() + text[1:])
+			else:
+				return None
+		else:
+			return cls.from_string(text, cls)
+
+class StackExchangeError(Exception):
+	"""A generic error thrown on a bad HTTP request during a StackExchange API request."""
+	def __init__(self, urlerror):
+		self.urlerror = urlerror
+	def __str__(self):
+		return 'Received HTTP error \'%d\'.' % self.urlerror.code
+
+
+class StackExchangeResultset(tuple):
+	"""Defines an immutable, paginated resultset. This class can be used as a tuple, but provides extended metadata as well, including methods
+to fetch the next page."""
+
+	def __new__(cls, items, page, pagesize, build_info):
+		cls.page, cls.pagesize, cls.build_info = page, pagesize, build_info
+		return tuple.__new__(cls, items)
+	
+	def reload(self):
+		"""Refreshes the data in the resultset with fresh API data. Note that this doesn't work with extended resultsets."""
+		# kind of a cheat, but oh well
+		return self.fetch_page(self.page)
+
+	def fetch_page(self, page, **kw):
+		"""Returns a new resultset containing data from the specified page of the results. It re-uses all parameters that were passed in
+to the initial function which created the resultset."""
+		new_params = list(self.build_info)
+		new_params[4] = new_params[4].copy()
+		new_params[4].update(kw)
+		new_params[4]['page'] = page
+		return new_params[0].build(*new_params[1:])
+	
+	def fetch_extended(self, page):
+		"""Returns a new resultset containing data from this resultset AND from the specified page."""
+		next = self.fetch_page(page)
+		extended = self + next
+
+		# max(0, ...) is so a non-zero, positive result for page is always found
+		return StackExchangeResultset(extended, max(1, self.page - 1), self.pagesize + next.pagesize, self.build_info)
+
+	def fetch_next(self):
+		"""Returns the resultset of the data in the next page."""
+		return self.fetch_page(self.page + 1)
+	
+	def extend_next(self):
+		"""Returns a new resultset containing data from this resultset AND from the next page."""
+		return self.fetch_extended(self.page + 1)
+
+class NeedsAwokenError(Exception):
+	"""An error raised when an attempt is made to access a property of a lazy collection that requires the data to have been fetched,
+but whose data has not yet been requested."""
+
+	def __init__(self, lazy):
+		self.lazy = lazy
+	def __str__(self):
+		return 'Could not return requested data; the sequence of "%s" has not been fetched.' % self.lazy.m_lazy
+
+class StackExchangeLazySequence(list):
+	"""Provides a sequence which *can* contain extra data available on an object. It is 'lazy' in the sense that data is only fetched when
+required - not on object creation."""
+
+	def __init__(self, m_type, count, site, url, fetch=None, collection=None):
+		self.m_type = m_type
+		self.count = count
+		self.site = site
+		self.url = url
+		self.fetch_callback = fetch
+		self.collection = collection if collection != None else self._collection(url)
+	
+	def _collection(self, c):
+		return c.split('/')[-1]
+
+	def __len__(self):
+		if self.count != None:
+			return self.count
+		else:
+			raise NeedsAwokenError(self)
+	
+	def fetch(self, **kw):
+		"""Fetch, from the API, the data this sequence is meant to hold."""
+
+		res = self.site.build(self.url, self.m_type, self.collection, kw)
+		if self.fetch_callback != None:
+			self.fetch_callback(res)
+		return res
+
+#### Hack, because I can't be bothered to fix my mistaking JSON's output for an object not a dict
+# Attrib: Eli Bendersky, http://stackoverflow.com/questions/1305532/convert-python-dict-to-object/1305663#1305663
+class DictObject:
+    def __init__(self, entries): 
+        self.__dict__.update(entries)
+
+
