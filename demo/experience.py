cf0c7aa3b26d06f0cc6cfb6a4e92ed8c196771f5
Lucas Jones <lucas@lucasjones.co.uk>
2013-11-11 18:00:24 +0000

    Fix 'Experience' demo.

--- demo/experience.py
+++ demo/experience.py
@@ -15,7 +15,7 @@ user = so.user(user_id)
 print 'Most experienced on %s.' % user.top_answer_tags.fetch()[0].tag_name
 print 'Most curious about %s.' % user.top_question_tags.fetch()[0].tag_name
 
-total_questions = len(user.questions)
+total_questions = len(user.questions.fetch())
 unaccepted_questions = len(user.unaccepted_questions.fetch())
 accepted = total_questions - unaccepted_questions
 rate = accepted / float(total_questions) * 100
